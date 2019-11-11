b06d61add0341275ed9e8393e3b1b31a62c82e1b
Anthony Pardo <pardo.anthony@gmail.com>
2019-11-11 21:28:13 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  hash = {}
+  names = []
+  data.each do |attribute, value|
+    value.each do |key, names|
+      names.each do |name|
+        names << name if !names.include?(name)
+        hash[name] = {
+          :color=>[],
+          :gender=>[],
+          :lives=>[]
+        }
+      end
+    end
+  end
+  
+  data.each do |attribute, value|
+    value.each do |key, names|
+      names.each do |name|
+        hash[name][attribute] << key.to_s
+      end
+    end
+  end
+  
+  
+  hash
 end
