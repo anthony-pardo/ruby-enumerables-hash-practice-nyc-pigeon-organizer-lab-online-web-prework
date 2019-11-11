be9a1529b876cd4ef9580850b689c2ae45202473
Anthony Pardo <pardo.anthony@gmail.com>
2019-11-11 20:26:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,13 +2,6 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   hash = {}
   data.each do |attribute, value|
-    data[attribute][value].each do |pigeon|
-      hash[pigeon] = {
-        :color=>[],
-        :gender=>[],
-        :lives=>[]
-      }
-    end
   end
   hash
 end
