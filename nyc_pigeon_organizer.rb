1e3a3d3a5821b97a92409ff8d3a21855c93e0d24
Anthony Pardo <pardo.anthony@gmail.com>
2019-11-11 21:26:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -37,7 +37,7 @@ def nyc_pigeon_organizer(data)
   data.each do |attribute, value|
     value.each do |key, names|
       names.each do |name|
-        hash[name][attribute]
+        hash[name][attribute] << key
       end
     end
   end
